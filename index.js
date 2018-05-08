9d4fdc574a21cbf8847fe952a946ea6085c6a831
Andrew Yates <andrew_yates@rocketmail.com>
2018-05-08 03:24:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -64,15 +64,9 @@ function removeFromCart(item) {
 	  if(cart[k]['itemName'] === item) {
 		  cart.splice(k, 1);
 		  return cart;
-    } 
-  }
-if(removed = 'yes') {
-	return cart;
-}
-else {
-	return ("That item is not in your cart.")
-
-}
+    } else {
+	      return ("That item is not in your cart.");
+      }
 }
 
 
