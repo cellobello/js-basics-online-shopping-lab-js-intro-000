659672e9042da49addba643adc857ff38f07d645
Andrew Yates <andrew_yates@rocketmail.com>
2018-05-08 04:02:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -77,6 +77,6 @@ function placeOrder(cardNumber) {
   if (arguments[0] === undefined) {
     return 'Sorry, we don\'t have a credit card on file for you.';
   } else {
-    return `Your total cost is 
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
   }
 }
