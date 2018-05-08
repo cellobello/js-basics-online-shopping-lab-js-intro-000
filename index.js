b2252c2b222b95b382d580084f89b066e58967de
Andrew Yates <andrew_yates@rocketmail.com>
2018-05-08 03:52:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -74,5 +74,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardnumber === null) {
+    return "Sorry, we don't have a credit card on file for you."
+  }
 }
